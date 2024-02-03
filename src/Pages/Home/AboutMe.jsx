1aa401e8ce2a6f9b36ee739895c80ed86dff776f
vindu9985 <abadavath68@gmail.com>
2024-02-03 13:21:03 -0600

    First commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,17 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          I am currently pursuing my masters in computer science at Texas A&M University. I am interested in Software Development, Full Stack Development, Backend Development, Object-Oriented Programming. I love developing solutions to real-world problems and love to work in cutting edge technologies and increasing my knowledge by learning new things.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
